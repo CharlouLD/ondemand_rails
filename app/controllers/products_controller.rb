a4c6e64a57c545f5ba72058e252db51be556973c
Charlou <Charlou@MacBook-de-Charles.local>
2016-04-11 15:08:28 +0200

    add resources routing for products and CRUD actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,21 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 
 end
