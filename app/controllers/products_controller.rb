40c5970092c2b567a453b318983cce7ef3355131
CharlouLD <c.legras@me.com>
2016-04-13 17:54:37 +0200

    added action update and destroy + refacto and form render

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
 class ProductsController < ApplicationController
 
+# voir private method
+before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
   end
 
   def new
@@ -22,19 +25,31 @@ class ProductsController < ApplicationController
   end
 
   def edit
+   # @product = Product.find(params[:id])
   end
 
   def update
+   # @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+   # @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
 private
 
-def product_params
-  params.require(:product).permit(:name, :url)
+  def product_params
+    params.require(:product).permit(:name, :url)
+  end
 
-end
-
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
